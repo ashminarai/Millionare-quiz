65577aeb487d935aadb9e90d42260c8a91748b73
ashmina <raiashmina404@gmail.com>
2023-07-15 20:51:53 +0545

    updated

---
+++ src/Components/Basics/Application.js
@@ -0,0 +1,92 @@
+import React, {useState} from "react";
+import "./Style.css";
+
+const Application = () => {
+
+  // creating useState Hook
+  const [questionNumber, setQuestionNumber] = useState(1)
+
+  // defining a React functional component called MoneyPyramid.
+  const moneyPyramid = [
+    {
+      id: 1,
+      amount: "$ 100",
+    },
+    {
+      id: 2,
+      amount: "$ 200",
+    },
+    {
+      id: 3,
+      amount: "$ 300",
+    },
+    {
+      id: 4,
+      amount: "$ 500",
+    },
+    {
+      id: 5,
+      amount: "$ 1000",
+    },
+    {
+      id: 6,
+      amount: "$ 2000",
+    },
+    {
+      id: 7,
+      amount: "$ 4000",
+    },
+    {
+      id: 8,
+      amount: "$ 8000",
+    },
+    {
+      id: 9,
+      amount: "$ 16000",
+    },
+    {
+      id: 10,
+      amount: "$ 32000",
+    },
+    {
+      id: 11,
+      amount: "$ 64000",
+    },
+    {
+      id: 12,
+      amount: "$ 125000",
+    },
+    {
+      id: 13,
+      amount: "$ 250000",
+    },
+    {
+      id: 14,
+      amount: "$ 500000",
+    },
+    {
+      id: 15,
+      amount: "$ 1000000",
+    },
+  ].reverse();
+  return (
+    <>
+      <div className="app">
+        <div className="main">main</div>
+        <div className="pyramid">
+          <ul className="moneyList">
+
+            {moneyPyramid.map((m)=>(
+              <li className={questionNumber === m.id ? "moneyListItem active" : "moneyListItem"}>
+              <span className="moneyListItemNumber">{m.id}</span>
+              <span className="moneyListItemAmount">{m.amount}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Application;
